8c034ff483ac1245578e7db36c64aee2983f3106
Daham-Mustaf <s7damust@uni-bonn.de>
2022-01-28 17:45:21 +0100

    functional sorting

--- src/main/java/com/java8/MapSorting.java
+++ src/main/java/com/java8/MapSorting.java
@@ -8,18 +8,23 @@ public class MapSorting {
         map.put(4, "Java");
         map.put(9, "C++");
         map.put(1, "R");
+        // traditional way
         List<Map.Entry<Integer, String>> entries = new ArrayList<>(map.entrySet());
-      Collections.sort(entries, new Comparator<Map.Entry<Integer, String>>() {
+        Collections.sort(entries, new Comparator<Map.Entry<Integer, String>>() {
           @Override
           public int compare(Map.Entry<Integer, String> o1, Map.Entry<Integer, String> o2) {
               return o1.getKey().compareTo(o2.getKey());
           }
-      });
+         });
       for(Map.Entry<Integer, String> s: entries){
           System.out.println(s);
       }
 
-        // traditional way
-
+      // functional way
+        List<Map.Entry<Integer, String>> entryList = new ArrayList<>(map.entrySet());
+        Collections.sort(entries,(o1,o2)-> o1.getKey().compareTo(o2.getKey()));
+        for(Map.Entry<Integer, String> s: entryList){
+            System.out.println(s);
+        }
     }
 }
