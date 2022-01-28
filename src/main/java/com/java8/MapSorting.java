e044c32c9013f65820a0655f1809630265e2ce97
Daham-Mustaf <s7damust@uni-bonn.de>
2022-01-28 17:29:23 +0100

    Sorting map

--- src/main/java/com/java8/MapSorting.java
+++ src/main/java/com/java8/MapSorting.java
@@ -1,13 +1,25 @@
 package com.java8;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MapSorting {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
-        map.put(1, "Java");
-        map.put(2, "C++");
-        map.put(3, "R");
+        map.put(4, "Java");
+        map.put(9, "C++");
+        map.put(1, "R");
+        List<Map.Entry<Integer, String>> entries = new ArrayList<>(map.entrySet());
+      Collections.sort(entries, new Comparator<Map.Entry<Integer, String>>() {
+          @Override
+          public int compare(Map.Entry<Integer, String> o1, Map.Entry<Integer, String> o2) {
+              return o1.getKey().compareTo(o2.getKey());
+          }
+      });
+      for(Map.Entry<Integer, String> s: entries){
+          System.out.println(s);
+      }
+
+        // traditional way
+
     }
 }
