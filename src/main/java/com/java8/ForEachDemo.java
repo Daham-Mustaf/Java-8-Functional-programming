4bc54cf7047ee193c72721ad96d701179866adde
Daham-Mustaf <s7damust@uni-bonn.de>
2022-01-28 13:26:49 +0100

    ForEachDemo

---
+++ src/main/java/com/java8/ForEachDemo.java
@@ -0,0 +1,17 @@
+package com.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForEachDemo {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("C++");
+        list.add("Python");
+        // traditional printing the list
+        for(String s : list){
+            System.out.println(s);
+        }
+    }
+}
