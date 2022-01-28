29cebb58147d61ec1fe196a2163c4ff4a00c3285
Daham-Mustaf <s7damust@uni-bonn.de>
2022-01-28 19:12:51 +0100

    MapReduceDemo

---
+++ src/main/java/com/java8/MapReduceDemo.java
@@ -0,0 +1,20 @@
+package com.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapReduceDemo {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(3, 7, 8, 1, 5, 9);
+        // summ of the numbers
+        int sum =0;
+        for(Integer in: numbers){
+            sum=sum+in;
+        }
+        System.out.println(sum);
+        // functional programming
+       int sumOfStream= numbers.stream().mapToInt(i->i).sum();
+        System.out.println(sumOfStream);
+
+    }
+}
