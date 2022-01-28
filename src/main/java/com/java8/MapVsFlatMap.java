f3629dc149b916bb35e6872303d34740ed772859
Daham-Mustaf <s7damust@uni-bonn.de>
2022-01-28 18:30:48 +0100

    flatting data

--- src/main/java/com/java8/MapVsFlatMap.java
+++ src/main/java/com/java8/MapVsFlatMap.java
@@ -11,6 +11,16 @@ public class MapVsFlatMap {
         List<Student> studentList = StudentDB.getAll();
         // list of Students to Strong list
         // one to one mapping
+        // email addresses are not a list of date can be mapped to string list
         studentList.stream().map(s->s.getEmail()).collect(Collectors.toList()).forEach(System.out::println);
+
+        List<List<String>> phons =studentList.stream().map(student -> student.getPhoneNumbers()).collect(Collectors.toList());
+        System.out.println(phons); // [[123, 342], [4555, 342], [123, 342]]
+
+        // flating data
+        List<String> phone=
+                studentList.stream().flatMap(student -> student.getPhoneNumbers().stream()).collect(Collectors.toList());
+        System.out.println(phone);
+
     }
 }
