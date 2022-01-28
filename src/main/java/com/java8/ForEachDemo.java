eb7840b7a36bccee320507485ab3b585b60e7955
Daham-Mustaf <s7damust@uni-bonn.de>
2022-01-28 13:32:33 +0100

    mapp printing with foreach

--- src/main/java/com/java8/ForEachDemo.java
+++ src/main/java/com/java8/ForEachDemo.java
@@ -1,7 +1,9 @@
 package com.java8;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ForEachDemo {
     public static void main(String[] args) {
@@ -15,5 +17,10 @@ public class ForEachDemo {
         }
         // java 8
         list.stream().forEach(System.out::println);
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Java");
+        map.put(2, "C++");
+        map.put(3, "R");
+        map.forEach((key, value)-> System.out.println(key + ": "+ value));
     }
 }
